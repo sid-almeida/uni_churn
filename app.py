35378703ea0734229da63501b01a2a0cfb630f10
Sidnei Almeida <117240480+sid-almeida@users.noreply.github.com>
2023-07-11 13:07:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -124,8 +124,8 @@ if choice == "Machine Learning":
                 from sklearn.linear_model import LogisticRegression
                 from sklearn.model_selection import train_test_split
                 from sklearn.metrics import accuracy_score
-                X = dataframe.drop('STATUS', axis=1)
-                y = dataframe['STATUS']
+                X = dataframe.drop(alvo, axis=1)
+                y = dataframe[alvo]
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
                 model = LogisticRegression()
                 model.fit(X_train, y_train)
@@ -149,8 +149,8 @@ if choice == "Machine Learning":
                 from sklearn.ensemble import RandomForestClassifier
                 from sklearn.model_selection import train_test_split
                 from sklearn.metrics import accuracy_score
-                X = dataframe.drop('STATUS', axis=1)
-                y = dataframe['STATUS']
+                X = dataframe.drop(alvo, axis=1)
+                y = dataframe[alvo]
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
                 model = RandomForestClassifier()
                 model.fit(X_train, y_train)
@@ -174,8 +174,8 @@ if choice == "Machine Learning":
                 from xgboost import XGBClassifier
                 from sklearn.model_selection import train_test_split
                 from sklearn.metrics import accuracy_score
-                X = dataframe.drop('STATUS', axis=1)
-                y = dataframe['STATUS']
+                X = dataframe.drop(alvo, axis=1)
+                y = dataframe[alvo]
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
                 model = XGBClassifier()
                 model.fit(X_train, y_train)
